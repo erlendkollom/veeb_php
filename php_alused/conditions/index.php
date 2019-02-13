33eed6388eecb16f3efe998727590c8baf475444
erlend.kollom <erlend.kollom@khk.ee>
2019-02-13 15:15:19 +0200

    Condition - paaris/paaritu arvu värviline väljastamine

--- php_alused/conditions/index.php
+++ php_alused/conditions/index.php
@@ -1,3 +1,20 @@
+<style>
+    div {
+        margin: auto;
+        width: 100px;
+        height: 100px;
+        font-size: 50px;
+        text-align: center;
+    }
+    .paaris{
+        background: red;
+
+    }
+
+    .paaritu {
+        background: green;
+    }
+</style>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,27 +22,16 @@
  * Date: 13.02.2019
  * Time: 14:14
  */
-$eesNimi = 'Erlend';
-$pereNimi = 'Kollom';
-$vanus = 21;
-$kaal = 90.1;
-$sugu = 'mees';
 
-switch ($sugu){
-    case 'mees':
-        echo '<p style="color: blue">';
-        break;
-    case 'naine':
-        echo '<p style="color: red">';
-        break;
-    default:
-        echo '<p style="color: green">';
-        break;
+//Genereerime arvu 0-100 vahel
+$arv = rand(0,100);
+//arvutame jäägi 2-ga jagamisel.
+$jaak = $arv % 2;
+if ($jaak == 0 ){
+    echo '<div class="paaris">'.$arv.'</div>';
 }
 
+else{
+    echo '<div class="paaritu">'.$arv.'</div>';
+    }
 
-echo 'Minu eesnimi on '.$eesNimi.'<br>';
-echo 'Minu perenimi on '.$pereNimi.'<br>';
-echo 'Minu vanus on '.$vanus.'<br>';
-echo 'Minu kaal on '.$kaal.'<br>';
-echo '</p>';
