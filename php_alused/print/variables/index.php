9c00188b50bc4b7042790ee895483f56d16c3c8d
erlend.kollom <erlend.kollom@khk.ee>
2019-02-13 14:21:35 +0200

    Variables - muutujate defineerimine

---
+++ php_alused/print/variables/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erlend.kollom
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesNimi = 'Erlend';
+$pereNimi = 'Kollom';
+$vanus = 21;
+$kaal = 90.1;
+
+//var_dump($eesNimi); //muutja sisu test vaatmine
+
+
