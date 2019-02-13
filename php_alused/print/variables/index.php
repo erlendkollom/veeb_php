64ead393adb991b2ed31e24111e948cfa63a439e
erlend.kollom <erlend.kollom@khk.ee>
2019-02-13 14:27:53 +0200

    Variables - väljastati muutujate väärtused

--- php_alused/print/variables/index.php
+++ php_alused/print/variables/index.php
@@ -12,4 +12,7 @@ $kaal = 90.1;
 
 //var_dump($eesNimi); //muutja sisu test vaatmine
 
-
+echo 'Minu eesnimi on '.$eesNimi.'<br>';
+echo 'Minu perenimi on '.$pereNimi.'<br>';
+echo 'Minu vanus on '.$vanus.'<br>';
+echo 'Minu kaal on '.$kaal.'<br>';
