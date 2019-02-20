16c9d10aa8e81e2fff02664b2935d4c2d6cf4198
erlend.kollom <erlend.kollom@khk.ee>
2019-02-20 14:01:10 +0200

    Functions - loodud tabeli väljastamiseks vajalikud funktsioonid

--- php_alused/Functions/index.php
+++ php_alused/Functions/index.php
@@ -1,26 +1,71 @@
 <?php
-// defineeririme funktioosni nimega paarsuseKontroll
+/**
+ * function paarsuseKontroll
+ * @param $arv
+ * print is $arv even or add
+ */
 function kasPaaris($arv){
-
-    if ($arv % 2 == 0) {
-       $kontroll = true;
+    if($arv % 2 == 0){
+        $kontroll = true;
     } else {
         $kontroll = false;
-        }
+    }
     return $kontroll;
 }
-
-function kirjeldus($paaris){
-    if($paaris){
-       echo ' on paaris';
+function kasAlgarv($arv){
+    $jagaja = 2;
+    if($arv == 0 or $arv == 1) {
+        $kontroll = true;
     } else {
-        echo ' on paaritu';
+        while ($arv % $jagaja != 0) $jagaja++;
+        if ($arv == $jagaja) {
+            $kontroll = true;
+        } else {
+            $kontroll = false;
+        }
     }
-    echo '<br>';
+    return $kontroll;
+}
+function tabeliPais(){
+    return '
+        <thead>
+                <tr>
+                    <th>arv</th>
+                    <th>paaris</th>
+                    <th>paaritu</th>
+                    <th>algarv</th>
+                </tr>
+        </thead>
+    ';
+}
+function tabeliRida($arv){
+    echo '<tr>';
+    echo '<td>'.$arv.'</td>';
+    echo '<td class="paaris">';
+    if(kasPaaris($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '<td class="paaritu">';
+    if(!kasPaaris($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '<td class="algarv">';
+    if(kasAlgarv($arv)){
+        echo '<i class="fas fa-angle-down"></i>';
+    }
+    echo '</td>';
+    echo '</tr>';
+}
+function tabel($ridadeArv){
+    echo '<table>';
+    echo tabeliPais();
+    echo '<tbody>';
+    for($reanumber = 0; $reanumber <= $ridadeArv; $reanumber++){
+        tabeliRida($reanumber);
+    }
+    echo '</tbody>';
+    echo '</table>';
 }
 
-// kutsume funktsiooni tööle
-for($arv = 0 ; $arv <=10; $arv++) {
-    echo $arv;
-    kirjeldus(kasPaaris($arv));
-}
--- php_alused/Loops/index.php
+++ php_alused/Loops/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sookla menuu</title>
+    <title></title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
