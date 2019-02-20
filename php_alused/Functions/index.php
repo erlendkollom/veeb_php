fe59de9cc98e3d03924826de4d5a2f66d6b6ef00
erlend.kollom <erlend.kollom@khk.ee>
2019-02-20 12:50:45 +0200

    Functions - paarsuse kontroll koos eraldi väljastamisega

--- php_alused/Functions/index.php
+++ php_alused/Functions/index.php
@@ -1,17 +1,27 @@
 <?php
 // defineeririme funktioosni nimega paarsuseKontroll
-function paarsuseKontroll($arv){
+function kasPaaris($arv){
 
     if ($arv % 2 == 0) {
-       $kontroll = $arv .' on paaris <br>';
+       $kontroll = true;
     } else {
-        $kontroll = $arv.' on paaritu <br>';
+        $kontroll = false;
         }
     return $kontroll;
 }
 
+function kirjeldus($paaris){
+    if($paaris == true ){
+       echo ' on paaris';
+    } else {
+        echo ' on paaritu';
+    }
+    echo '<br>';
+}
+
 // kutsume funktsiooni tööle
 for($arv = 0 ; $arv <=10; $arv++) {
-    $kontroll = paarsuseKontroll($arv);
-    echo $kontroll;
+    echo $arv;
+    $kontroll = kasPaaris($arv);
+    kirjeldus($kontroll);
 }
