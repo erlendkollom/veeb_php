3e98a24794e572b0e151edc4387775d279c33d87
erlend.kollom <erlend.kollom@khk.ee>
2019-02-25 12:31:35 +0200

    Form - Vormi andmete kontroll

--- php_alused/form/vormi_tootlus.php
+++ php_alused/form/vormi_tootlus.php
@@ -2,8 +2,12 @@
 
 extract($_GET);
 
-/*$nimi = $_GET['nimi'];
-$parool = $_GET['parool'];*/
+//$nimi = $_GET['nimi'];
+//$parool = $_GET['parool'];
 
-echo 'Tere '.$nimi.'!<br>';
-echo 'Sinu parooliks on '.$parool;
+if (strlen($nimi) == 0 or strlen($parool) == 0 ){
+    header('Location: index.html');
+} else {
+    echo 'Tere '.$nimi.'!<br>';
+    echo 'Sinu parooliks on '.$parool;
+}
