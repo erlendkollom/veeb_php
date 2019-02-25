490545cf1d4cefc1fc1cec63e6ca71fbf8902340
erlend.kollom <erlend.kollom@khk.ee>
2019-02-25 08:56:18 +0200

    Array - sidestaud massiivi loomine ja kasutamine

--- php_alused/Array/index.php
+++ php_alused/Array/index.php
@@ -1,40 +1,15 @@
 <?php
-
-$porsad = array(
-    array(
-        'Peppa',
-        'naine',
-        4,
-        1.04
-    ),
-    array(
-        'George',
-        'mees',
-        2,
-        0.95
-    )
+$peppa = array(
+    'nimi' => 'Peppa',
+    'sugu' => 'naine',
+    'vanus' => 4,
+    'pikkus' => 1.04
 );
-
-
-echo $porsad[0][0] .'<br>';
-echo $porsad[0][1].'<br>';
-echo $porsad[0][2].'<br>';
-echo $porsad[0][3].'<br>';
-
-echo '<hr>';
-
-for($i = 0; $i < count($porsad) ; $i++ ){
-    for ($j = 0; $j < count($porsad[$i]); $j++){
-        echo $porsad [$i] [$j].'<br>';
-    }
-}
-
-echo '<hr>';
-
-foreach ($porsad as $porsas) {
-    foreach ($porsas as $element) {
-        echo $element . '<br>';
-    }
-    echo '<hr>';
-}
-
+$george = array(
+    'nimi' => 'George',
+    'sugu' => 'mees',
+    'vanus' => 2,
+    'pikkus' => 0.95
+);
+echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
+echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
