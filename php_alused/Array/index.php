b1a17f61a5be35c496eebe531b0868f91443f38f
erlend.kollom <erlend.kollom@khk.ee>
2019-02-25 10:46:36 +0200

    Array - Raamatute massiivid, koos loodud tabeli päisega

--- php_alused/Array/index.php
+++ php_alused/Array/index.php
@@ -1,43 +1,54 @@
 <?php
 
-
+$raamatud = array(
 
 $raamatUks = array(
     'pealkiri' => 'Kui paanikahoog ründab',
     'hind' => 18.90,
     'keel' => 'eesti',
     'autor' => 'Aine Tubridy'
-);
+),
 
 $raamatKaks = array(
     'pealkiri' => 'Eesti side ja interneti 100 aastat',
     'hind' => 15.00,
     'keel' => 'eesti',
     'autor' => 'Jaak Ulman'
-);
+),
 
 $raamatKolm = array(
     'pealkiri' => 'Toskaana aastat',
     'hind' => 26.95,
     'keel' => 'eesti',
     'autor' => 'Elizabeth Romer'
-);
+),
 
 $raamatNeli = array(
     'pealkiri' => 'Jänku-Jassi kingitus',
     'hind' => 7.95,
     'keel' => 'eesti',
     'autor' => 'Julia Sigarova'
+)
 );
 
-$raamatud = array();
-$raamatud['Esimene raamat'] = $raamatUks;
-$raamatud['Teine raamat'] = $raamatKaks;
-$raamatud['Kolmas raamat'] = $raamatKolm;
-$raamatud['Neljas raamat'] = $raamatNeli;
+function tabeliPais($andmed){
+    echo '<thead>';
+        echo '<tr>';
+        foreach ($andmed as $element){
+            echo '<th>'.$element.'</th>';
+        }
+        echo '</tr>';
+    echo '</thead>';
+}
+
+$tabeli_pealkiri = array_keys($raamatud[0]);
+echo '<table border="1">';
+tabeliPais($tabeli_pealkiri);
+echo  '</tabel>';
 
 
-echo '<hr>';
+
+
 
 
 
@@ -56,5 +67,5 @@ foreach ($raamatud as $raamatuPealkiri=>$raamatuAndmed){
 }
 
 
-
+echo '<hr>';
 
