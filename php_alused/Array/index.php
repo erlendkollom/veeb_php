ad13438dceec70fff2678744973afa26cf42f8d5
erlend.kollom <erlend.kollom@khk.ee>
2019-02-25 09:14:29 +0200

    Array - sidestatud massiivi loomine ja kasutamine

--- php_alused/Array/index.php
+++ php_alused/Array/index.php
@@ -11,5 +11,27 @@ $george = array(
     'vanus' => 2,
     'pikkus' => 0.95
 );
+foreach ($peppa as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
+}
+echo '<hr>';
+foreach ($george as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
+}
+echo '<hr>';
 echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
 echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
+echo '<hr>';
+$porsad = array();
+$porsad['peppa'] = $peppa;
+$porsad['george'] = $george;
+$porsad['peppa']['lemmik varv'] = 'punane';
+$porsad['george']['lemmik varv'] = 'sinine';
+foreach ($porsad as $porsaseNimi=>$porsaseAndmed){
+    echo '<b>'.$porsaseNimi.'</b><br>';
+    echo '<ul>';
+    foreach ($porsaseAndmed as $nimetus=>$vaartus){
+        echo '<li>'.$nimetus.' - '.$vaartus.'</li>';
+    }
+    echo '</ul>';
+}
