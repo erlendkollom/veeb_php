2400793902b3561bbb17a4ad865725697e7877b7
erlend.kollom <erlend.kollom@khk.ee>
2019-03-04 10:20:23 +0200

    Date - Kuupäeva arvutamine

--- php_alused/date/index.php
+++ php_alused/date/index.php
@@ -1,9 +1,27 @@
 <?php
 
+$eesti_kuud = array(
+  2=> 'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember'
+);
 
+$eesti_paevad = array(
+    1=>'esmaspäev',
+        'teisipäev',
+        'kolmapäev',
+        'neljapäev',
+        'reede',
+        'laupäev',
+        'pühapäev'
+);
+$paevSonaga = $eesti_paevad[date('n')];
+$paevNumbriga =date('d');
+$aasta = date('Y');
+$kuu = $eesti_kuud[date('n')];
 
-if(checkdate(13,29,2049)) {
-    echo('Kuupäev korras!');
-} else {
-    echo ('Kuupäev on valesti sisestatud');
-}
+$now = time(); // praegune kellaaeg
+//jaanipäev
+$your_date = strtotime("2019-06-24");
+//arvutan välja ajavahe
+$datediff = $your_date - $now;
+//ümardan arvu päevadeni
+echo round($datediff / (60 * 60 * 24));
