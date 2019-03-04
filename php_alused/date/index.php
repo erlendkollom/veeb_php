7607f60bdd71d107dbd8179898d2f20edcc15e21
erlend.kollom <erlend.kollom@khk.ee>
2019-03-04 10:04:10 +0200

    Date - Kuupäeva kontrollimine

--- php_alused/date/index.php
+++ php_alused/date/index.php
@@ -1,26 +1,9 @@
 <?php
-//kuude massiiv
 
-$eesti_kuud = array(
-    1=> 'jaanuar',
-        'veebruar',
-        'märts',
-        'aprill',
-        'mai',
-        'juuni',
-        'juuli',
-        'august',
-        'september',
-        'oktoober',
-        'november',
-        'detsember'
-);
 
-//kuupäevad massiividesse
 
-$paev = date('d');
-$kuu = $eesti_kuud[date('n')];
-$aasta = date('Y');
-//kuupäeva väljastamine
-
-echo $paev.'.'.$kuu.' '.$aasta;
+if(checkdate(13,29,2049)) {
+    echo('Kuupäev korras!');
+} else {
+    echo ('Kuupäev on valesti sisestatud');
+}
