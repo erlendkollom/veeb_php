1e1cd2693372a55acb217c7dd98ba2108d22e2c8
Erlend <erlend.kollom@khk.ee>
2019-03-11 15:17:30 +0200

    obejct-oriented PHP - classes and objects

--- Object-oriented PHP/Class.php
+++ Object-oriented PHP/Class.php
@@ -1,14 +1,45 @@
 <?php
+// Declare the class
+class Car {
 
-class Car
-{
+    // properties
     public $comp;
     public $color = 'beige';
     public $hasSunRoof = true;
+
+    // method that says hello
+    public function hello()
+    {
+        return "beep";
+    }
 }
 
+// Create an instance
 $bmw = new Car ();
 $mercedes = new Car ();
 
-echo $bmw -> color;
-echo $mercedes -> color;
+// Get the values
+echo $bmw -> color; // beige
+echo "<br />";
+echo $mercedes -> color; // beige
+echo "<hr />";
+
+// Set the values
+$bmw -> color = 'blue';
+$bmw -> comp = "BMW";
+$mercedes -> comp = "Mercedes Benz";
+
+// Get the values again
+echo $bmw -> color; // blue
+echo "<br />";
+echo $mercedes -> color; // beige
+echo "<br />";
+echo $bmw -> comp; // BMW
+echo "<br />";
+echo $mercedes -> comp; // Mercedes Benz
+echo "<hr />";
+
+// Use the methods to get a beep
+echo $bmw -> hello(); // beep
+echo "<br />";
+echo $mercedes -> hello(); // beep
