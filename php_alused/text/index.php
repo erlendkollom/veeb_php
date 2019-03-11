209f8303e2440f531097d59d6b9785ee5ec6c8a4
erlend.kollom <erlend.kollom@khk.ee>
2019-03-11 08:50:43 +0200

    db&sessions

--- php_alused/text/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$sisendUks = "Mario";
-$sisendKaks = "karl";
-$sisendKolm = "mArKoO";
-
-echo ucwords(strtolower("Tere $sisendUks!")).'<br>';
-echo ucwords(strtolower("Tere $sisendKaks!")).'<br>';
