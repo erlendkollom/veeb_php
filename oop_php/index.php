528937a446dcd0a9a331dbd5785a03b26f9d2e37
erlend.kollom <erlend.kollom@khk.ee>
2019-03-11 09:48:35 +0200

    oop - lisatud pealehe näidis sisu

--- oop_php/index.php
+++ oop_php/index.php
@@ -19,5 +19,12 @@ $main->set('title', 'App Title');
 // add action control
 // require_once 'controller.php';
 
+$mainContent= new Template('main_content');
+$mainContent->set('header', 'Page header');
+$mainContent->set('nav', 'Page navigation');
+$mainContent->set('content', 'Page content');
+$mainContent->set('footer', 'Page footer');
+$main->set('content', $mainContent->parse());
+
 // print out main page full view
 echo $main->parse();
